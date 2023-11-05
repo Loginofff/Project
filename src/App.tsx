59b9c001a164a8cf1c6200d0338595680cb4c5bb
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2023-11-05 19:13:35 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function App() {
+  return (
+    <div className="App">
+     Hallo
+    </div>
+  )
+}
+
+export default App
